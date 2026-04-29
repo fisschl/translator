acdafbb12aeca7ec6c2256f82fa83fbb0865541e
Fisschl <fisschl@163.com>
2026-04-29 16:39:52 +0800

    chore(mock): remove mock configuration and related files

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,38 @@
 import { fileURLToPath, URL } from "node:url";
 import ui from "@nuxt/ui/vite";
 import vue from "@vitejs/plugin-vue";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import VueRouter from "vue-router/vite";
 
-export default defineConfig(({ mode }) => {
-  const { VITE_SERVER_PROXY_TARGET } = loadEnv(mode, fileURLToPath(new URL("./", import.meta.url)));
-  return {
-    base: "/translator/",
-    plugins: [
-      VueRouter({
-        exclude: ["**/utils/**", "**/components/**", "**/assets/**"],
-        extensions: [".vue"],
-      }),
-      vue(),
-      ui(),
-    ],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig({
+  base: "/translator/",
+  plugins: [
+    VueRouter({
+      exclude: ["**/utils/**", "**/components/**", "**/assets/**"],
+      extensions: [".vue"],
+    }),
+    vue(),
+    ui(),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    transformer: "lightningcss",
+    lightningcss: {
+      cssModules: {
+        pure: true,
       },
     },
-    css: {
-      transformer: "lightningcss",
-      lightningcss: {
-        cssModules: {
-          pure: true,
-        },
+  },
+  server: {
+    proxy: {
+      "/api/chat": {
+        target: "https://bronya.world",
+        changeOrigin: true,
       },
     },
-    server: {
-      proxy: {
-        "/api/chat": {
-          target: VITE_SERVER_PROXY_TARGET,
-          changeOrigin: true,
-        },
-      },
-    },
-  };
+  },
 });
